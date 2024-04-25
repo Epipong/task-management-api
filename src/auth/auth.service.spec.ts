724bd8d3d44feeaa52ef23b74e1303c2fcaa20ee
davy.tran <davytran1993@gmail.com>
2024-04-25 15:32:49 +0200

    Add test to auth for wrong credential

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { UsersRepository } from 'src/repositories/users.repertory';
 import { PrismaService } from 'src/prisma/prisma.service';
 import user from 'src/fixtures/users';
+import { BadRequestException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -29,5 +30,18 @@ describe('AuthService', () => {
     };
     const token = await authService.login(credential);
     expect(token.access_token).toBeTruthy();
+  });
+
+  it('should not receive a token', async () => {
+    const invalidCredential = {
+      email: 'tran.pauline@club.com',
+      password: 'Abcdef123@',
+    };
+    try {
+      await authService.login(invalidCredential);
+    } catch (error) {
+      expect(error).toBeInstanceOf(BadRequestException);
+      expect(error.message).toBe('email or password are invalid');
+    }
   });
 });
